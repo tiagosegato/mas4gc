0ba415fe47a95d4783ff5ef996b6d0c2e689510b
Tiago Segato <tiago.segato@ifb.edu.br>
2021-04-20 09:36:32 -0300

    datas to dataframe

--- agents/models/paa.py
+++ agents/models/paa.py
@@ -9,7 +9,8 @@ import connection
 from datetime import datetime
 import pickle
 import numpy as np
-from scipy import stats
+import pandas as pd
+import json
 from sklearn.linear_model import LinearRegression
 
 
@@ -43,53 +44,73 @@ class PAAgent(Agent):
 def relatorioAvaliacao(self):
     # consultando dados específicos do último paciente cadastrado
     document = connection.collection.find({},{"_id": 1, "nome": 1, "sexo": 1, "imc": 1, "diabetes": 1, 
-    "glicemia.valorGlicemia": 1, "glicemia.dataHoraColeta": 1, "glicemia.ultimaAlimentacao": 1}).sort("updateDate", -1).limit(1)
+    "glicemia.valorGlicemia": 1, "glicemia.dataHoraColeta": 1, "glicemia.ultimaAlimentacao": 1})
+
+    dataframe = pd.DataFrame(columns=['codigo','sexo', 'imc', 'diabetes', 'tempos', 'hora', 'alimentacao', ' ultimaglicemia', 'glicemia'])
     
-    # dados recuperados na consulta acima
-    idPaciente = document[0]["_id"]
-    paciente = document[0]["nome"]
-    sexo = document[0]["sexo"]
-    sexoBin = lambda sexo : int(1) if sexo == "Masculino" else int(0)
-    sexo = sexoBin(sexo)
-    imc = round(document[0]["imc"])
-    diabetes = document[0]["diabetes"]
-    diabetesBin = lambda diabetes : int(0) if diabetes == "Não tem" else (int(1) if diabetes == "Ignorado" else int(2))
-    diabetes = diabetesBin(diabetes)
+    codigoPaciente = 0
+    for p in document:
 
-    coletas = len(document[0]["glicemia"])
+        # dados recuperados na consulta acima
+        codigoPaciente = codigoPaciente + 1
+        idPaciente = p["_id"]
+        paciente = p["nome"]
 
-    glicemias = document[0]["glicemia"]
-    valoresGlicemia = [int(g['valorGlicemia']) for g in glicemias] # pegando os valores das glicemias 
+        sexo = p["sexo"]
+        sexoBin = lambda sexo : int(1) if sexo == "Masculino" else int(0)
+        sexo = sexoBin(sexo)
+
+        imc = round(p["imc"])
+
+        diabetes = p["diabetes"]
+        diabetesBin = lambda diabetes : int(0) if diabetes == "Não tem" else (int(1) if diabetes == "Ignorado" else int(2))
+        diabetes = diabetesBin(diabetes)
+
+        coletas = len(p["glicemia"])
+
+        glicemias = p["glicemia"]
+        valoresGlicemia = [int(g['valorGlicemia']) for g in glicemias] # pegando os valores das glicemias 
+        
+        if (coletas >0):
+            ultimas = valoresGlicemia
+            ultimaGlicemia = ultimas[:-1]
+            ultimaGlicemia.insert(0,99)
+            #ultimaGlicemia = int(valoresGlicemia[coletas-2]) #pega apenas o
+        else: ultimaGlicemia = 'Não coletada!'
+
+        horario = p["glicemia"]
+        horariosColeta = [h['dataHoraColeta'] for h in horario] # pegando os horários das coletas
+        horaColeta=[]
+        for h in horariosColeta:
+            hc = datetime.strptime(h, "%Y-%m-%d %H:%M")
+            horaColeta.append(int(hc.strftime('%H')))
+
+        # convertendo os horários em horas
+        temposColeta = []
+        for x in horariosColeta:
+            # diferença entre as horas...
+            diferenca = (datetime.strptime(x, '%Y-%m-%d %H:%M')
+                        - datetime.strptime(horariosColeta[0], '%Y-%m-%d %H:%M')).total_seconds() / 3600 #convertendo em horas
+                
+            horaUltimaColeta = diferenca # pega apenas a última coleta
+            temposColeta.append(int(diferenca)) # adiciona os horários das coletas no array
+        
+        alimentacao = p["glicemia"]
+        ultimaAlimentacao = [int(a['ultimaAlimentacao']) for a in horario] # pegando as ultimas alimentações
+
+        ### CRIANDO O DATAFRAME ###
+        for i in range(coletas):
+            df = pd.DataFrame({'codigo':codigoPaciente,'sexo':sexo, 'imc':imc, 'diabetes':diabetes, 'tempos':temposColeta[i], 'hora':horaColeta[i], 
+                            'alimentacao':ultimaAlimentacao[i], ' ultimaglicemia':ultimaGlicemia[i], 'glicemia':valoresGlicemia[i]}, index=[0])
+            dataframe = pd.concat([dataframe, df], ignore_index=True)
+            dataframe.reset_index()
+            i+=1
 
 
-    if (coletas >0):
-        ultimas = valoresGlicemia
-        ultimaGlicemia = ultimas[:-1]
-        ultimaGlicemia.insert(0,99)
-        #ultimaGlicemia = int(valoresGlicemia[coletas-2]) #pega apenas o
-    else: ultimaGlicemia = 'Não coletada!'
-
-    horario = document[0]["glicemia"]
-    horariosColeta = [h['dataHoraColeta'] for h in horario] # pegando os horários das coletas
-    horaColeta=[]
-    for h in horariosColeta:
-        hc = datetime.strptime(h, "%Y-%m-%d %H:%M")
-        horaColeta.append(int(hc.strftime('%H')))
-
-    # convertendo os horários em minutos
-    temposColeta = []
-    for x in horariosColeta:
-        # diferença entre as horas...
-        diferenca = (datetime.strptime(x, '%Y-%m-%d %H:%M')
-                    - datetime.strptime(horariosColeta[0], '%Y-%m-%d %H:%M')).total_seconds() / 3600 #convertendo em horas
-            
-        horaUltimaColeta = diferenca # pega apenas a última coleta
-        temposColeta.append(int(diferenca)) # adiciona os horários das coletas no array
+    print(dataframe)
     
-    alimentacao = document[0]["glicemia"]
-    ultimaAlimentacao = [int(a['ultimaAlimentacao']) for a in horario] # pegando as ultimas alimentações
-
     # exibindo os dados coletados
+    '''
     print('')
     print('DADOS PACIENTE')
     print('ID Paciente: ', idPaciente)
@@ -107,7 +128,26 @@ def relatorioAvaliacao(self):
     print('Última Glicemia: ', ultimaGlicemia)
     #print('Coletas: ', horariosColeta)
     print('')
+    '''
 
+    ###### 2
+    '''
+    pacientes = connection.collection.find({},{"_id": 1, "nome": 1, "sexo": 1, "imc": 1, "diabetes": 1, 
+    "glicemia.valorGlicemia": 1, "glicemia.dataHoraColeta": 1, "glicemia.ultimaAlimentacao": 1})
+
+    pacientes = list(pacientes)
+    
+    df = pd.DataFrame(list(pacientes.items()),columns = ['column1','column2','column3','column4','column5','column6','column7','column8']) 
+    print(df)
+    '''
+
+
+
+            
+   
+    
+
+    ### VERIFICA A QUANTIDADE DE GLICEMIAS COLETADAS ###
     # verifica se existem glicemias para aquele paciente    
     if coletas == 0:
         situacao = "semGlicemia" 
